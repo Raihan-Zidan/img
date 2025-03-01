b6ae9648d04d308489c2291cfd28f601e8f5baee
Raihan-Zidan <105942900+Raihan-Zidan@users.noreply.github.com>
2025-03-01 17:04:54 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,11 @@
 export default {
-  async fetch(request, env) {
-    // Ambil file WASM dari Cloudflare Worker
-    const wasmModule = await WebAssembly.instantiate(env.libjpeg);
+  async fetch(request, env, ctx) {
+    // Load file WASM secara langsung
+    const wasmResponse = await fetch(new URL("./djpeg-static.wasm", import.meta.url));
+    const wasmArrayBuffer = await wasmResponse.arrayBuffer();
+    const wasmModule = await WebAssembly.instantiate(wasmArrayBuffer);
 
     return new Response("WASM loaded successfully!", { status: 200 });
   },
 };
+
